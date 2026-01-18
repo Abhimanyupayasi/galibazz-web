bd0b30fe7e0eb0c26c6710e2723402fe9cc24a50
Abhimanyu <abhimanyupayasi01@gmail.com>
2026-01-18 13:58:41 +0530

    added

--- app/sitemap2.xml/route.js
+++ app/sitemap2.xml/route.js
@@ -2,34 +2,19 @@ export async function GET() {
   const urls = [
     "https://galibazz.lol/",
     "https://galibazz.lol/en",
-    "https://galibazz.lol/hi",
-    "https://galibazz.lol/en/jokes/hindi-jokes",
-    "https://galibazz.lol/hi/jokes/hindi-jokes",
-    "https://galibazz.lol/en/jokes/kabir-ke-dohe-adult",
-    "https://galibazz.lol/hi/jokes/kabir-ke-dohe-adult",
-    "https://galibazz.lol/en/jokes/double-meaning-shayari",
-    "https://galibazz.lol/hi/jokes/double-meaning-shayari",
-    "https://galibazz.lol/en/jokes/teacher-student-jokes",
-    "https://galibazz.lol/hi/jokes/teacher-student-jokes"
+    "https://galibazz.lol/hi"
   ];
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls
-      .map(
-        (url) => `
-        <url>
-          <loc>${url}</loc>
-          <lastmod>${new Date().toISOString()}</lastmod>
-        </url>
-      `
-      )
-      .join("")}
-  </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map(url => `<url><loc>${url}</loc></url>`).join("")}
+</urlset>`;
 
   return new Response(xml, {
+    status: 200,
     headers: {
-      "Content-Type": "application/xml",
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=3600"
     },
   });
 }
