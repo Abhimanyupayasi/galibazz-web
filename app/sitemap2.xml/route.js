508d3cf3abd31caeb21e734c969e05bfbbd1b565
Abhimanyu <abhimanyupayasi01@gmail.com>
2026-01-18 14:00:26 +0530

    added

--- app/sitemap2.xml/route.js
+++ app/sitemap2.xml/route.js
@@ -5,16 +5,15 @@ export async function GET() {
     "https://galibazz.lol/hi"
   ];
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls.map(url => `<url><loc>${url}</loc></url>`).join("")}
 </urlset>`;
 
-  return new Response(xml, {
+  return new Response(body, {
     status: 200,
     headers: {
-      "Content-Type": "application/xml; charset=utf-8",
-      "Cache-Control": "public, max-age=3600"
-    },
+      "Content-Type": "application/xml; charset=UTF-8"
+    }
   });
 }
