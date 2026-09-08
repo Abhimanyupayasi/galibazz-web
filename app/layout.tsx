67525145263f6c907a7270d14bb6ad82e1d17351
Abhimanyu Payasi <117967027+Abhimanyupayasi@users.noreply.github.com>
2026-09-08 10:01:29 +0530

    Integrate Google Ads script in layout
    Added Google Ads script for ad integration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -119,6 +119,8 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com" rel="preconnect" />
         <link crossOrigin="anonymous" href="https://fonts.gstatic.com" rel="preconnect" />
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4665029971539939"
+     crossorigin="anonymous"></script>
       </head>
       <body
   suppressHydrationWarning
